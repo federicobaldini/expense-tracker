d6a6f5db5d088a5e8c78b545ff4b35bda7a51146
federico <federico-baldini@hotmail.com>
2021-10-25 13:41:57 +0200

    modified ExpenseItem component and style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
 
-    return (
-        <h2>Expense Item!</h2>
-    );
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
