c02d413f165f7e496d7ebe9410d3a2b1faced1f7
federico <federico-baldini@hotmail.com>
2021-10-25 21:21:37 +0200

    new container component Card and ExpenseItem fix

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString('it-IT', { month: 'long' });
-  const day = props.date.toLocaleString('it-IT', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-    </div>
+    </Card>
   );
 }
 
