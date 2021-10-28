458245baf82766803a8b6f5f56eb3e0e87ee7051
federico <federico-baldini@hotmail.com>
2021-10-28 23:38:49 +0200

    Card component minor changes

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,9 @@
 import "./Card.css";
 
 const Card = (props) => {
-    const classes = 'card ' + props.className;
+  const classes = "card " + props.className;
 
-    return (
-        <div className={classes}>{props.children}</div>
-    );
-}
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
