c25831528f85dd23a3fbfc151202643f510f991a
federico <federico-baldini@hotmail.com>
2021-10-30 12:29:50 +0200

    added new component ExpenseFilter

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,26 @@
+import "./ExpenseFilter.css";
+
+const ExpenseFilter = (props) => {
+  const yearsFilter = ["2018", "2019", "2020", "2021", "2022"];
+
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          {yearsFilter.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
