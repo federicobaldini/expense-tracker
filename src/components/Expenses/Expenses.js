64d8f0576fdf4b11a9f919ff7ec732450fd180e0
federico <federico-baldini@hotmail.com>
2021-10-30 12:31:22 +0200

    added filtered items list to Expense component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,36 @@
 import "./Expenses.css";
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
-      {props.items.map((expense) => (
-        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-      ))}
+      <ExpenseFilter
+        onChangeFilter={filterChangeHandler}
+        selected={filteredYear}
+      />
+      {props.items
+        .filter(
+          (expense) => expense.date.getFullYear().toString() === filteredYear
+        )
+        .map((filteredExpense) => (
+          <ExpenseItem
+            key={filteredExpense.id}
+            title={filteredExpense.title}
+            amount={filteredExpense.amount}
+            date={filteredExpense.date}
+          />
+        ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
