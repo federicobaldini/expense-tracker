dbda80390c77799512b557c77b4f24be9637dba2
federico <federico-baldini@hotmail.com>
2021-11-06 10:44:25 +0100

    added React Portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,21 +1,52 @@
 import { Fragment } from "react";
+import ReactDOM from "react-dom";
 import classes from "./ErrorModal.module.css";
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className={classes["error-modal__backdrop"]}
+      onClick={props.onConfirm}
+    />
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes["error-modal__modal"]}>
+      <header className={classes["error-modal__header"]}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes["error-modal__content"]}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes["error-modal__actions"]}>
+        <button
+          className={classes["error-modal__button"]}
+          onClick={props.onConfirm}
+        >
+          OK
+        </button>
+      </footer>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
     <Fragment>
-      <div className={classes['error-modal__backdrop']} onClick={props.onConfirm} />
-      <div className={classes['error-modal__modal']}>
-        <header className={classes['error-modal__header']}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes['error-modal__content']}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes['error-modal__actions']}>
-          <button className={classes['error-modal__button']} onClick={props.onConfirm}>Okay</button>
-        </footer>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
