a09f0c46d3e14fdc88f90515d10d11decc82dd67
federico <federico-baldini@hotmail.com>
2021-11-08 21:46:34 +0100

    minor changes

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,15 +1,15 @@
 import classes from "./ExpenseDate.module.css";
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("it-IT", { month: "long" });
-  const day = props.date.toLocaleString("it-IT", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
-    <div className={classes['expense-date']}>
-      <div className={classes['expense-date__month']}>{month}</div>
-      <div className={classes['expense-date__year']}>{year}</div>
-      <div className={classes['expense-date__day']}>{day}</div>
+    <div className={classes["expense-date"]}>
+      <div className={classes["expense-date__month"]}>{month}</div>
+      <div className={classes["expense-date__year"]}>{year}</div>
+      <div className={classes["expense-date__day"]}>{day}</div>
     </div>
   );
 };
