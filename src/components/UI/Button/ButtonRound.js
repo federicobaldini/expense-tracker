ebed191b160f93296690952a63b99a83fc6911c7
federico <federico-baldini@hotmail.com>
2021-11-10 21:10:08 +0100

    added new UI components

---
+++ src/components/UI/Button/ButtonRound.js
@@ -0,0 +1,16 @@
+import classes from './ButtonRound.module.css';
+
+const ButtonRound = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes['button']} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default ButtonRound;
---
+++ src/components/UI/Button/ButtonSquare.js
@@ -0,0 +1,16 @@
+import classes from './ButtonSquare.module.css';
+
+const ButtonSquare = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes['button']} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default ButtonSquare;
