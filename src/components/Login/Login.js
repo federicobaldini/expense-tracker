540d70bda0674dbe15a3ecf3ec9706dc232370c9
federicobaldini <federico-baldini@hotmail.com>
2021-11-25 11:47:22 +0100

    added new Input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import classes from "./Login.module.css";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -80,36 +81,26 @@ const Login = (props) => {
   return (
     <Card className={classes["login"]}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes["control"]} ${
-            emailState.isValid === false ? classes["invalid"] : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes["control"]} ${
-            passwordState.isValid === false ? classes["invalid"] : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          id="password"
+          label="Password"
+          type="text"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes["actions"]}>
-          <Button type="submit" disabled={!formIsValid}>
+          <Button className={classes["actions__button"]} type="submit" disabled={!formIsValid}>
             Login
           </Button>
         </div>
